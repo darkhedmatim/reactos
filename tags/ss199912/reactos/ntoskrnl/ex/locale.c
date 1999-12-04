c2e59a5243039934aee695399741415a26d883a6
nobody <nobody@97493ccd-5924-5043-b1f5-66cb403b36ce>
1999-12-04 08:02:27 +0000

    This commit was manufactured by cvs2svn to create tag 'ss199912'.

---
+++ tags/ss199912/reactos/ntoskrnl/ex/locale.c
@@ -0,0 +1,28 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS kernel
+ * FILE:            ntoskrnl/ex/locale.c
+ * PURPOSE:         Locale support
+ * PROGRAMMER:      David Welch (welch@cwcom.net)
+ * UPDATE HISTORY:
+ *                  Created 22/05/98
+ */
+
+/* INCLUDES *****************************************************************/
+
+#include <ddk/ntddk.h>
+
+#include <internal/debug.h>
+
+
+/* FUNCTIONS *****************************************************************/
+
+NTSTATUS STDCALL NtQueryDefaultLocale(VOID)
+{
+   UNIMPLEMENTED;
+}
+
+NTSTATUS STDCALL NtSetDefaultLocale(VOID)
+{
+   UNIMPLEMENTED;
+}
