042ada071d1eab8bcf6b38e1d011e2bc1674e0cd
nobody <nobody@97493ccd-5924-5043-b1f5-66cb403b36ce>
2001-11-24 08:35:44 +0000

    This commit was manufactured by cvs2svn to create tag 'ss200111'.

---
+++ tags/ss200111/reactos/lib/crtdll/signal/xcptfil.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+
+int _XcptFilter (
+         DWORD ExceptionCode,
+         struct _EXCEPTION_POINTERS *  ExceptionInfo 
+        )
+{
+	//fixme XcptFilter
+//	return UnhandledExceptionFilter(ExceptionInfo);
+}
