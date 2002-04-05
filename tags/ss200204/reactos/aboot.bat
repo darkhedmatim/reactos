56bf5149a205a8eb11f8ca2231e65ccc703038f1
nobody <nobody@97493ccd-5924-5043-b1f5-66cb403b36ce>
2002-04-05 12:53:21 +0000

    This commit was manufactured by cvs2svn to create tag 'ss200204'.

---
+++ tags/ss200204/reactos/aboot.bat
@@ -0,0 +1,1 @@
+loadros system32\ntoskrnl.exe system32\hal.dll system32\drivers\acpi.sys bootc.lst
---
+++ tags/ss200204/reactos/lib/crtdll/tchar/strspnp.c
@@ -0,0 +1,8 @@
+#include <crtdll/tchar.h>
+#include <crtdll/stdlib.h>
+
+
+char * _strspnp( const char * str1, const char * str2) 
+{ 
+	return NULL; 
+}
