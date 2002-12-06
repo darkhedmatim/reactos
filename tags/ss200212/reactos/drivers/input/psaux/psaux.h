bb4c83eec821912f677d4c8d618794efb4852d5b
nobody <nobody@97493ccd-5924-5043-b1f5-66cb403b36ce>
2002-12-06 21:39:04 +0000

    This commit was manufactured by cvs2svn to create tag 'ss200212'.

---
+++ tags/ss200212/reactos/drivers/input/psaux/psaux.h
@@ -0,0 +1,13 @@
+typedef struct _DEVICE_EXTENSION {
+
+  PDEVICE_OBJECT DeviceObject;
+
+  ULONG ActiveQueue;
+  ULONG InputDataCount[2];
+  MOUSE_INPUT_DATA MouseInputData[2][MOUSE_BUFFER_SIZE];
+  
+  CLASS_INFORMATION ClassInformation;
+  
+  PKINTERRUPT MouseInterrupt;
+   KDPC IsrDpc;
+} DEVICE_EXTENSION, *PDEVICE_EXTENSION;
