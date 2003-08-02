5eab9c91df28353ddf0b367fd77dac8f7b2c01ac
nobody <nobody@97493ccd-5924-5043-b1f5-66cb403b36ce>
2003-08-02 01:54:24 +0000

    This commit was manufactured by cvs2svn to create tag 'ss200308'.

---
+++ tags/ss200308/reactos/subsys/win32k/include/guicheck.h
@@ -0,0 +1,12 @@
+#ifndef __WIN32K_GUICHECK_H
+#define __WIN32K_GUICHECK_H
+
+#include <windows.h>
+#include <ddk/ntddk.h>
+
+VOID FASTCALL
+W32kGraphicsCheck(BOOL Create);
+
+#endif /* __WIN32K_GUICHECK_H */
+
+/* EOF */
