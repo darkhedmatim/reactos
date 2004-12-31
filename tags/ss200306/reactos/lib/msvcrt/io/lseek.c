df100ded1e21667d508530e728a749d7bc9ceea1
chorns <chorns@97493ccd-5924-5043-b1f5-66cb403b36ce>
2004-12-31 15:09:05 +0000

    Remove ss200306 tag.

--- tags/ss200306/reactos/lib/msvcrt/io/lseek.c
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-#include <msvcrt/io.h>
-#include <msvcrt/internal/file.h>
-
-
-long _lseek(int _fildes, long _offset, int _whence)
-{
-    return (SetFilePointer((HANDLE)filehnd(_fildes), _offset, NULL, _whence));
-}
