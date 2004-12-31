3c31c8e977a8e72e0fb1345be610c3c7f7d5024a
chorns <chorns@97493ccd-5924-5043-b1f5-66cb403b36ce>
2004-12-31 14:53:28 +0000

    Remove merged-into-trunk-1 tag.

--- tags/merged-into-trunk-1/reactos/lib/crtdll/io/eof.c
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-#include <io.h>
-
-int _eof( int _fd )
-{
-	return 0;
-}
--- tags/merged-into-trunk-1/reactos/lib/crtdll/io/pipe.c
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-#include <io.h>
-
-int _pipe(int _fildes[2], unsigned int size, int mode )
-{
-	return -1;
-}
