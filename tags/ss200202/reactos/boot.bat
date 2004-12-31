ff850531c0cc3135d0ffe235b30d3866994ef16b
chorns <chorns@97493ccd-5924-5043-b1f5-66cb403b36ce>
2004-12-31 15:03:00 +0000

    Remove ss200202 tag.

--- tags/ss200202/reactos/boot.bat
+++
@@ -1,1 +0,0 @@
-loadros system32\ntoskrnl.exe system32\hal.dll system32\drivers\ide.sys system32\drivers\vfatfs.sys
--- tags/ss200202/reactos/lib/msvcrt/io/dup.c
+++
@@ -1,8 +0,0 @@
-#include <msvcrt/io.h>
-#include <msvcrt/internal/file.h>
-
-
-int _dup(int handle)
-{
-  return __fileno_alloc(_get_osfhandle(handle), __fileno_getmode(handle));
-}
