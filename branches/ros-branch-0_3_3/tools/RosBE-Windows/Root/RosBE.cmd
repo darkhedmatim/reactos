802dcebe94e58f13c14c706193a1fb575b2d5c02
zguo <zguo@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-08-13 22:54:24 +0000

    Properly revert 28327-8 and 28330

---
+++ branches/ros-branch-0_3_3/tools/RosBE-Windows/Root/RosBE.cmd
@@ -0,0 +1,98 @@
+::
+:: PROJECT:     RosBE - ReactOS Build Environment for Windows
+:: LICENSE:     GPL - See COPYING in the top level directory
+:: FILE:        Root/RosBE.cmd
+:: PURPOSE:     This script provides/sets up various build environments for
+::              ReactOS. Currently it provides GCC 4.1.3 build environments.
+:: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+::                             Peter Ward <dralnix@gmail.com>
+::
+::
+@echo off
+
+set _VER=0.3.7
+set ROSBEBASEDIR=%~dp0
+
+title ReactOS Build Environment %_VER%
+
+if "%1" == "" (
+    color 0A
+    cls
+    call :RosBE4
+    goto :EndCommandParse
+)
+if not "%1" == "" (
+    color 0A
+    cls
+    echo Unknown parameter specified. Exiting.
+    goto :ExitRosBE
+)
+
+:EndCommandParse
+
+::
+:: Save our initial directory (should be the ReactOS source directory) 
+:: and add PATH for Tools Folder.
+::
+set _ROSSOURCEDIR=%CD%
+set _ROSSRCDIRBCK=%CD%
+set PATH=%ROSBEBASEDIR%\Tools;%PATH%
+
+::
+:: Display the current version of GCC, NASM, ld and make.
+::
+gcc -v 2>&1 | find "gcc version"
+nasm -v
+ld -v
+mingw32-make -v | find "GNU Make"
+
+::
+:: Tell how to display the available commands.
+::
+echo.
+echo For a list of all included commands, type: "help"
+echo -------------------------------------------------
+
+::
+:: Load the macros that serve as our commands.
+::
+doskey /macrofile="%ROSBEBASEDIR%\RosBE.mac"
+
+::
+:: Look if the Source Folder is empty. If so, ask for using "svn create".
+::
+dir /b "%_ROSSOURCEDIR%" 2>nul|grep -e ".*" >nul
+if errorlevel 1 (
+    echo No ReactOS Source detected. Please use "ssvn create" to download it.
+    goto :ExitRosBE
+) else (
+    goto :ExitRosBE
+)
+
+goto :ExitRosBE
+
+::
+:: Display the banner and set up the environment for the GCC 4.x.x build
+:: environment.
+::
+:RosBE4
+    ::
+    :: Set the correct path for the build tools and set the MinGW make.
+    ::
+    set PATH=%ROSBEBASEDIR%\4.1.3\bin;%ROSBEBASEDIR%\4.1.3\libexec\gcc\mingw32\4.1.3;%PATH%
+    set _MINGWMAKE=%ROSBEBASEDIR%\4.1.3\bin\mingw32-make.exe
+    set _MINGWVERSION=4.1.3
+    :: Set all paths (needed by Windows Vista)
+    set C_INCLUDE_PATH=%ROSBEBASEDIR%\4.1.3\include;%ROSBEBASEDIR%\4.1.3\lib\gcc\mingw32\4.1.3\include
+    set CPLUS_INCLUDE_PATH=%ROSBEBASEDIR%\4.1.3\include;%ROSBEBASEDIR%\4.1.3\include\c++\4.1.3;%ROSBEBASEDIR%\4.1.3\include\c++\4.1.3\mingw32;%ROSBEBASEDIR%\4.1.3\lib\gcc\mingw32\4.1.3\include
+    set LIBRARY_PATH=%ROSBEBASEDIR%\4.1.3\lib;%ROSBEBASEDIR%\4.1.3\lib\gcc\mingw32\4.1.3
+
+    echo *******************************************************************************
+    echo *                                                                             *
+    echo *        ReactOS Build Environment 0.3.7-4.1.3-20070703 (prerelease)          *
+    echo *                                                                             *
+    echo *******************************************************************************
+    echo.
+goto :EOF
+
+:ExitRosBE
---
+++ branches/ros-branch-0_3_3/tools/RosBE-Windows/Root/sSVN.cmd
@@ -0,0 +1,70 @@
+::
+:: PROJECT:     RosBE - ReactOS Build Environment for Windows
+:: LICENSE:     GPL - See COPYING in the top level directory
+:: FILE:        Root/SVN.cmd
+:: PURPOSE:     Integrated SVN Client.
+:: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+::
+::
+@echo off
+
+::
+:: Receive the first Parameter and decide what to do.
+::
+if "%1" == "" (
+    echo No parameter specified. Try 'help [COMMAND]'.
+    goto :ExitSVN
+)
+if "%1" == "update" (
+    title Updating...
+    if not "%2" == "" (
+        svn update "%_ROSSOURCEDIR%" -r %2
+    ) else (
+        svn update "%_ROSSOURCEDIR%"
+    )
+goto :ExitSVN
+)
+if "%1" == "cleanup" (
+    title Cleaning...
+    svn cleanup "%_ROSSOURCEDIR%"
+    goto :ExitSVN
+)
+::
+:: Check if the Folder is empty. If not, output an error.
+::
+
+if "%1" == "create" (
+    title Creating...
+    if exist "%_ROSSOURCEDIR%\.svn\." (
+        echo Folder already cotains a Reposority. Exiting
+        goto :ExitSVN
+    )
+    dir /b "%_ROSSOURCEDIR%" 2>nul|grep -e ".*" >nul
+    if errorlevel 1 (
+        svn checkout svn://svn.reactos.org/reactos/trunk/reactos "%_ROSSOURCEDIR%"
+    ) else (
+        echo Folder is not empty. Continuing is dangerous and can cause errors. ABORTED
+    )
+    goto :ExitSVN
+)
+if "%1" == "status" (
+    title Status
+    call svn info "%_ROSSOURCEDIR%" > "%ROSBEBASEDIR%\x.tmp"
+    echo Recent Offline Revision:
+    call grep Revision: "%ROSBEBASEDIR%\x.tmp"|cut -d " " -f 2
+    del "%ROSBEBASEDIR%\x.tmp"
+
+    call svn info svn://svn.reactos.org/reactos/trunk/reactos > "%ROSBEBASEDIR%\x.tmp"
+    echo Online HEAD Revision:
+    call grep Revision: "%ROSBEBASEDIR%\x.tmp"|cut -d " " -f 2
+    del "%ROSBEBASEDIR%\x.tmp"
+    goto :ExitSVN
+)
+
+if not "%1" == "" (
+    echo Unknown parameter specified. Try 'help [COMMAND]'.
+    goto :ExitSVN
+)
+
+:ExitSVN
+title ReactOS Build Environment %_VER%
