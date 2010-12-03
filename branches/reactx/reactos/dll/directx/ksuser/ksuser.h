b61355e3f702c647d2b522aa31e1f431d5d56af9
greatlrd <greatlrd@97493ccd-5924-5043-b1f5-66cb403b36ce>
2010-12-03 09:19:53 +0000

    all code are in ReactOS trunk for ReactX delete the old branch and prepare create new one Most of the code will be focus on dxg.sys and few part of gdi32.dll in the new branch of reactx

--- branches/reactx/reactos/dll/directx/ksuser/ksuser.h
+++
@@ -1,5 +0,0 @@
-#define _KSDDK_
-#include <windows.h>
-typedef LONG NTSTATUS;
-#include <ks.h>
-#include <reactos/helper.h>
--- branches/reactx/reactos/dll/win32/winsta/winsta.h
+++
@@ -1,7 +0,0 @@
-#if !defined(_WINSTA_H)
-#define _WINSTA_H
-
-#define WINSTASTUB(n) VOID WINAPI n (VOID) { }
-
-#endif /* !def _WINSTA_H */
-
