darkhedmatim/reactos

commit 5fb000a2e418eda2902590edeb784c592a4ead42

[NTOSKRNL] - Reference the cache segment when flushing to avoid a race between deleting the file cache and the balancer flushing pages to disk which causes random cache segments to be freed while we're holding a lock on it - Fixes a crash in KeReleaseMutex (used to be a crash in ExReleasePushLock) during periods of high memory load
svn path=/trunk/; revision=54745

reactos/ntoskrnl/cc/view.c
@@ -204,10 +204,13 @@ CcRosFlushDirtyPages(ULONG Target, PULONG Count, BOOLEAN Wait)
                                     DirtySegmentListEntry);
         current_entry = current_entry->Flink;
 
+        CcRosCacheSegmentIncRefCount(current);
+
         Locked = current->Bcb->Callbacks->AcquireForLazyWrite(
             current->Bcb->LazyWriteContext, Wait);
         if (!Locked)
         {
+            CcRosCacheSegmentDecRefCount(current);
             continue;
         }
 
@@ -220,6 +223,7 @@ CcRosFlushDirtyPages(ULONG Target, PULONG Count, BOOLEAN Wait)
         {
             current->Bcb->Callbacks->ReleaseFromLazyWrite(
                 current->Bcb->LazyWriteContext);
+            CcRosCacheSegmentDecRefCount(current);
             continue;
         }
 
@@ -229,6 +233,7 @@ CcRosFlushDirtyPages(ULONG Target, PULONG Count, BOOLEAN Wait)
             KeReleaseMutex(&current->Mutex, 0);
             current->Bcb->Callbacks->ReleaseFromLazyWrite(
                 current->Bcb->LazyWriteContext);
+            CcRosCacheSegmentDecRefCount(current);
             continue;
         }
 
@@ -241,6 +246,9 @@ CcRosFlushDirtyPages(ULONG Target, PULONG Count, BOOLEAN Wait)
         KeReleaseMutex(&current->Mutex, 0);
         current->Bcb->Callbacks->ReleaseFromLazyWrite(
             current->Bcb->LazyWriteContext);
+        
+        KeAcquireGuardedMutex(&ViewLock);
+        CcRosCacheSegmentDecRefCount(current);
 
         if (!NT_SUCCESS(Status) &&  (Status != STATUS_END_OF_FILE))
         {
@@ -251,8 +259,7 @@ CcRosFlushDirtyPages(ULONG Target, PULONG Count, BOOLEAN Wait)
             (*Count) += PagesPerSegment;
             Target -= PagesPerSegment;
         }
-        
-        KeAcquireGuardedMutex(&ViewLock);
+
         current_entry = DirtySegmentListHead.Flink;
     }
     

commit 093e2b0e934477ba7e13508e6beb946ec7ba57b0

[NTOSKRNL] - Fix a reference count check in the flush path (broken by r54745)

reactos/ntoskrnl/cc/view.c
@@ -228,7 +228,9 @@ CcRosFlushDirtyPages(ULONG Target, PULONG Count, BOOLEAN Wait)
         }
 
         ASSERT(current->Dirty);
-        if (current->ReferenceCount > 1)
+
+        /* One reference is added above */
+        if (current->ReferenceCount > 2)
         {
             KeReleaseMutex(&current->Mutex, 0);
             current->Bcb->Callbacks->ReleaseFromLazyWrite(