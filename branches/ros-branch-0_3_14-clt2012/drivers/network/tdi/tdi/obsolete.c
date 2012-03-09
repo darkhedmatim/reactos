6403338d34907bf92b25422eb624f2eb4a1c8085
tfaber <tfaber@97493ccd-5924-5043-b1f5-66cb403b36ce>
2012-03-09 19:00:33 +0000

    [CLT2012] - Create a branch for the 0.3.14-CLT2012 version

---
+++ branches/ros-branch-0_3_14-clt2012/drivers/network/tdi/tdi/obsolete.c
@@ -0,0 +1,32 @@
+/* $Id$
+ *
+ */
+
+#include "precomp.h"
+
+/*
+ * @unimplemented
+ */
+VOID
+NTAPI
+TdiMapBuffer (
+	IN PMDL MdlChain
+	)
+{
+	/* This function is absolete */
+}
+
+
+/*
+ * @unimplemented
+ */
+VOID
+NTAPI
+TdiUnmapBuffer (
+	IN PMDL MdlChain
+	)
+{
+	/* This function is obsolete */
+}
+
+/* EOF */
