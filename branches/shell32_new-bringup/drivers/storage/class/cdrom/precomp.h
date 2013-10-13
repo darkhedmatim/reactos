e9f68018f6cdc5b49fc33bc89b22ab3f5ecb6520
akhaldi <akhaldi@97493ccd-5924-5043-b1f5-66cb403b36ce>
2013-10-13 21:28:33 +0000

    [BRANCHES] * Remove the shell32_new-bringup branch (merged).

--- branches/shell32_new-bringup/drivers/storage/class/cdrom/precomp.h
+++
@@ -1,7 +0,0 @@
-#include <ntddk.h>
-#include <scsi.h>
-#include <ntddscsi.h>
-#include <ntdddisk.h>
-#include <ntddcdrm.h>
-#include <include/class2.h>
-#include <stdio.h>
--- branches/shell32_new-bringup/lib/drivers/sound/soundblaster/precomp.h
+++
@@ -1,4 +0,0 @@
-#include <ntddk.h>
-#include <debug.h>
-#include <time.h>
-#include <sbdsp.h>
