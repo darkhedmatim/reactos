82eea59bd4848cc2d493a7425dc23e3d777180f0
akhaldi <akhaldi@97493ccd-5924-5043-b1f5-66cb403b36ce>
2013-10-13 21:40:14 +0000

    [BRANCHES] * icu4ros-bringup: empty branch. * jcatena-branch: the ongoing work was superseded by MSVC support in trunk. * lean-explorer: old and inactive. * nyadav-audio-branch: Failed GSoC 2011 project. * arwinss: who cares about Aleksey anyway.. * dib_rewrite: who cares about Timo anyway..

--- branches/icu4ros-bringup/lib/sdk/crt/oldnames.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT MSVC)
-    add_custom_command(
-        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/liboldnames.a
-        COMMAND ${CMAKE_DLLTOOL} --def ${CMAKE_CURRENT_SOURCE_DIR}/moldname-msvcrt.def --kill-at --output-lib ${CMAKE_CURRENT_BINARY_DIR}/liboldnames.a)
-
-    set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/liboldnames.a PROPERTIES GENERATED TRUE)
-
-    add_custom_target(oldnames ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/liboldnames.a)
-endif()
--- branches/nyadav-audio-branch/dll/win32/audsrvapi/audsrvapi.spec
+++
@@ -1,8 +0,0 @@
-@ stdcall InitStream (ptr long long long long long long long long)
-@ stdcall PlayAudio ( ptr);
-@ stdcall StopAudio (ptr );
-@ stdcall Volume(ptr ptr );
-@ stdcall SetVolume(ptr long);
-@ stdcall Write(ptr ptr);
-@ stdcall SetBalance(ptr long);
-@ stdcall GetBalance(ptr ptr);
