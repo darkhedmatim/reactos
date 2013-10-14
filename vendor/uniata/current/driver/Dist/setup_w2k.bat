f066c9d3bc5c355fd1f0624631e19ed8a812656d
akhaldi <akhaldi@97493ccd-5924-5043-b1f5-66cb403b36ce>
2013-10-14 16:02:03 +0000

    [VENDOR] * Remove some old/unused vendor drops.

--- vendor/uniata/current/driver/Dist/setup_w2k.bat
+++
@@ -1,6 +0,0 @@
-echo off
-copy uniata.sys "%SystemRoot%\System32\drivers"
-start /wait regedit -s uniata_w2k.reg
-echo Universal ATA driver installed
-echo please reboot your computer to complete installation
-pause
