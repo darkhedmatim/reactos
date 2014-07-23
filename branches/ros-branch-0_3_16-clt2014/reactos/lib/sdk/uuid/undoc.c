6879a13a617d2e2b6a4cc780ecf39d6bd275a238
tfaber <tfaber@97493ccd-5924-5043-b1f5-66cb403b36ce>
2014-07-23 12:39:02 +0000

    [CLT2014] - ... and remove the branch

--- branches/ros-branch-0_3_16-clt2014/reactos/lib/sdk/uuid/undoc.c
+++
@@ -1,8 +0,0 @@
-
-#ifndef INITGUID
-#define INITGUID
-#endif
-#include <guiddef.h>
-
-#include <shlguid_undoc.h>
-
--- branches/ros-branch-0_3_16-clt2014/reactos/subsystems/ntvdm/resource.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define RC_STRING_MAX_SIZE 2048
-#define STRING_WelcomeMsg  100
-#define STRING_PromptMsg   101
